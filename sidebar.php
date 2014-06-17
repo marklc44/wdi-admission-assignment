c0e11033844e761ef39083e058ea3192566e7c99
Mark Centoni <marklc44@gmail.com>
2014-06-16 17:14:46 -0700

    trimmed out unneeded WP template files

--- sidebar.php
+++
@@ -1,9 +0,0 @@
-<div class="col-md-3 sidebar sidebar-offcanvas">
-
-  <?php if ( ! dynamic_sidebar( 'page' ) ): ?>
-
-  <h3>Sidebar</h3>
-  <p>Please add widgets to the sidebar to have them show up here.</p>
-  
-  <?php endif; ?>
-</div>
